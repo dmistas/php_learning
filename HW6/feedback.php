219dcc8eae1b39ce34f77f3f93f0c80711e867dc
Stanislav Dmitriev <dmistas@gmail.com>
2020-09-12 20:24:48 +0500

    hw6 done t1-2

---
+++ HW6/feedback.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Feedback</title>
+</head>
+<body>
+
+
+    <div id="feedbacks">
+        <h3>Прошлые отзывы</h3>
+        
+    </div>
+    <hr>
+    <div id="feedbac-form">
+        <form action="">
+            <p>Как вас зовут?</p>
+            <input type="text" name="fio">
+            <p>Ваш отзыв:</p>
+            <textarea name="info" id="" cols="30" rows="10"></textarea>
+        </form>
+    </div>
+</body>
+</html>
